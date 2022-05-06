a77def555bba5f6d35a3f88e159441f4d6564389
gorettii <101406498+gorettii@users.noreply.github.com>
2022-05-06 14:03:04 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (1 == receivedNumber) {
+        basic.showString("A")
+    }
+    if (2 == receivedNumber) {
+        basic.showString("B")
+    }
+    if (3 == receivedNumber) {
+        basic.showString("B+A")
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+})
+radio.setGroup(20)
+basic.forever(function () {
+	
+})
